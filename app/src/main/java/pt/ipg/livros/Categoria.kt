5d071e699ea110a0da0651cac64416223580e028
hugomdinis <hugomdinis@homtail.com>
2022-05-27 10:15:51 +0100

    Criar função para conter valores

--- app/src/main/java/pt/ipg/livros/Categoria.kt
+++ app/src/main/java/pt/ipg/livros/Categoria.kt
@@ -1,5 +1,13 @@
 package pt.ipg.livros
 
-data class Categoria(var id: Long, var nome: String) {
+import android.content.ContentValues
 
+data class Categoria(var id: Long, var nome: String) {
+  fun toContentValies() : ContentValues{
+      val valores = ContentValues()
+
+      valores.put(TabelaBDCategorias.CAMPO_NOME, nome)
+
+      return valores
+  }
 }
---
+++ app/src/main/java/pt/ipg/livros/Livro.kt
@@ -0,0 +1,9 @@
+package pt.ipg.livros
+
+data class Livros (
+    var id: Long,
+    var titulo: String,
+
+    var categoriaId: Long
+        ) {
+}
