579e430734e1995952b6680e8bc7172ccfa06204
hugomdinis <hugomdinis@homtail.com>
2022-05-27 10:17:45 +0100

    Criar função para conter valores da tabela livros

--- app/src/main/java/pt/ipg/livros/Livro.kt
+++ app/src/main/java/pt/ipg/livros/Livro.kt
@@ -1,9 +1,20 @@
 package pt.ipg.livros
 
+import android.content.ContentValues
+
 data class Livros (
     var id: Long,
     var titulo: String,
-
+    var autores: String,
     var categoriaId: Long
         ) {
+    fun toContentValies() : ContentValues {
+        val valores = ContentValues()
+
+        valores.put(TabelaBDLivros.CAMPO_TITULO, titulo)
+        valores.put(TabelaBDLivros.CAMPO_AUTOR, autores)
+        valores.put(TabelaBDLivros.CAMPO_CATEGORIA_ID, categoriaId)
+
+        return valores
+    }
 }
